7a59618d9800b313684b11c2d4e4bc0e258eb673
Asterios Patsikas <sergiodk5@gmail.com>
2025-04-22 00:35:11 +0300

    fix: temp disable eslint

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,6 +2,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const SERVER_PORT = process.env.PORT ?? 8090;
-export const DATABASE_URL = process.env.DATABASE_URL ?? 'postgres://postgres:postgres@localhost:5432/postgres';
-export const JWT_SECRET = process.env.JWT_SECRET ?? 'your_jwt_secret' as string;
+const getEnv = (key: string, defaultValue: string): string => {
+    const value = process.env[key] ?? defaultValue;
+
+    if (value === undefined) {
+        throw new Error(`Environment variable ${key} is not defined`);
+    }
+
+    return value;
+};
+
+export const SERVER_PORT = getEnv("PORT", "8090");
+export const DATABASE_URL = getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/postgres");
+export const JWT_SECRET = getEnv("JWT_SECRET", "your_jwt_secret");
+export const JWT_REFRESH_SECRET = getEnv("JWT_REFRESH_SECRET", "your_jwt_refresh_secret");
